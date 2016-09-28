57f48fe7361d88f83be0ca8c53fff7cd4edd3549
Markus Svilans <msvilans@aeonyx.ca>
2016-09-28 10:07:20 -0400

    Experimenting with labels

--- src/main.c
+++ src/main.c
@@ -37,6 +37,10 @@ static Evas_Object *createContent2(Evas_Object *parent);
 EAPI_MAIN int
 elm_main(int argc, char **argv)
 {
+    //elm_config_focus_highlight_enabled_set(EINA_FALSE);
+    elm_config_focus_move_policy_set(EFL_UI_FOCUS_MOVE_POLICY_IN);
+    elm_config_window_auto_focus_enable_set(EINA_FALSE);
+
     // Use global key callbacks for testing
     ecore_event_handler_add(ECORE_EVENT_KEY_DOWN, keyEventCallback, NULL);
     // ecore_event_handler_add(ECORE_EVENT_KEY_UP, keyEventCallback, NULL);
@@ -44,6 +48,7 @@ elm_main(int argc, char **argv)
 
     win = elm_win_add(NULL, "Focus Test", ELM_WIN_BASIC);
     elm_win_autodel_set(win, EINA_TRUE);
+    elm_win_focus_highlight_enabled_set(win, EINA_FALSE);
     evas_object_resize(win, 200, 150);
     evas_object_show(win);
 
@@ -79,10 +84,10 @@ What should happen is that this callback gets called for key down events
 while Content 1 is visible in the flip
 */
 static void
-contentKeyDownCallback1(void *data, Evas *e EINA_UNUSED,
+content1_KeyDownCallback(void *data, Evas *e EINA_UNUSED,
     Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-    Ecore_Event_Key *ev = (Ecore_Event_Key *)event_info;
+    Evas_Event_Key_Down *ev = event_info;
     const char *k = ev->keyname;
     printf("Content 1 key down: %s\n", ev->keyname);
 
@@ -91,12 +96,56 @@ contentKeyDownCallback1(void *data, Evas *e EINA_UNUSED,
     }
 }
 
+// static void
+// content1_FocusInCallback(void *data, Evas *e EINA_UNUSED,
+//     Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
+// {
+//     printf("content1 focused\n");
+// }
+
+// static void
+// content1_FocusOutCallback(void *data, Evas *e EINA_UNUSED,
+//     Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
+// {
+//     printf("content1 unfocused\n");
+// }
+
 static void
-contentKeyCallback2(void *data, Evas *e EINA_UNUSED,
+contentKeyDownCallback2(void *data, Evas *e EINA_UNUSED,
     Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-    Ecore_Event_Key *ev = (Ecore_Event_Key *)event_info;
+    Evas_Event_Key_Down *ev = event_info;
     printf("Content 2 key press: %s\n", ev->keyname);
+}
+
+static void
+content1_FocusedCallback(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
+{
+    elm_win_focus_highlight_enabled_set(win, EINA_FALSE);
+    printf("content1 focused\n");
+}
+
+static void
+content1_UnfocusedCallback(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
+{
+    // elm_win_focus_highlight_enabled_set(win, EINA_TRUE);
+    printf("content1 unfocused\n");
+}
+
+static void
+content2_FocusedCallback(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
+{
+}
+
+static void
+content2_UnfocusedCallback(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
+{
+}
+
+static void
+table_FocusedCallback(void *data EINA_UNUSED, Evas_Object *obj, void *event_info EINA_UNUSED)
+{
+    printf("table focused\n");
 }
 
 static Evas_Object *createContent1(Evas_Object *parent)
@@ -105,12 +154,13 @@ static Evas_Object *createContent1(Evas_Object *parent)
     Evas_Object *table = NULL;
     Evas_Object *rect = NULL;
     Evas_Object *label = NULL;
+    Evas_Object *button = NULL;
 
     table = elm_table_add(parent);
     evas_object_show(table);
 
-    label = elm_label_add(win);
-    evas_object_size_hint_weight_set(rv, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    label = elm_label_add(parent);
+    evas_object_size_hint_weight_set(label, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
     elm_object_text_set(label,
         "Content 1"
         "<br><br>Press 'f' to flip"
@@ -122,23 +172,44 @@ static Evas_Object *createContent1(Evas_Object *parent)
     This part is based on ephoto_single_browser.c lines 2195-2203
     Thanks to okra on IRC #edevelop
     */
-    rect = evas_object_rectangle_add(parent);
-    evas_object_smart_member_add(rect, parent);
-    evas_object_color_set(rect, 128, 0, 0, 255);
+    // rect = evas_object_rectangle_add(parent);
+    // evas_object_smart_member_add(rect, parent);
+    rect = elm_label_add(parent);
+    elm_object_focus_allow_set(rect, EINA_TRUE);
+    evas_object_size_hint_weight_set(rect, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    // evas_object_smart_member_add(rect, parent);
+    evas_object_color_set(rect, 0, 0, 0, 0);
     //evas_object_size_hint_weight_set(rect, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-    evas_object_repeat_events_set(rect, EINA_TRUE);
+    // evas_object_repeat_events_set(rect, EINA_TRUE);
 
     // TODO: rectangle does not appear visible... shouldn't it?
-    evas_object_move(rect, 50, 50);
-    evas_object_resize(rect, 100, 100);
+    // evas_object_move(rect, 50, 50);
+    // evas_object_resize(rect, 100, 100);
     evas_object_show(rect);
     evas_object_event_callback_add(rect, EVAS_CALLBACK_KEY_DOWN,
-        contentKeyDownCallback1, NULL);
+        content1_KeyDownCallback, NULL);
+    // evas_object_event_callback_add(rect, EVAS_CALLBACK_FOCUS_IN,
+    //     content1_FocusInCallback, NULL);
+    // evas_object_event_callback_add(rect, EVAS_CALLBACK_FOCUS_IN,
+    //     content1_FocusOutCallback, NULL);
+    
+    evas_object_smart_callback_add(rect, "focused", content1_FocusedCallback, NULL);
+    evas_object_smart_callback_add(rect, "unfocused", content1_UnfocusedCallback, NULL);
 
     // with or without this, no difference
-    elm_object_focus_set(rect, EINA_TRUE);
+    // elm_object_focus_set(rect, EINA_FALSE);
+    // elm_object_focus_set(rect, EINA_TRUE);
     elm_table_pack(table, rect, 0, 0, 1, 1);
     evas_object_raise(rect);
+
+    elm_object_focus_allow_set(table, EINA_TRUE);
+    evas_object_smart_callback_add(table, "focused", table_FocusedCallback, NULL);
+    elm_object_focus_set(table, EINA_TRUE);
+
+    // button = elm_button_add(parent);
+    // elm_object_text_set(button, "Test Button");
+    // evas_object_show(button);
+    // elm_table_pack(table, button, 0, 1, 1, 1);
 
     rv = table;
 
@@ -149,8 +220,11 @@ static Evas_Object *createContent2(Evas_Object *parent)
 {
     Evas_Object *rv = NULL; // return value
     rv = elm_label_add(win);
+    elm_object_focus_allow_set(rv, EINA_TRUE);
     evas_object_size_hint_weight_set(rv, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
     elm_object_text_set(rv, "Content 2<br><br>Press 'f' to flip");
     evas_object_show(rv);
+    evas_object_event_callback_add(rv, EVAS_CALLBACK_KEY_DOWN,
+        contentKeyDownCallback2, NULL);
     return rv;
 }
